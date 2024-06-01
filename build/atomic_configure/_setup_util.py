28a901e7846644dcb75994c0453dd098cff41712
doldolmeng2 <ktypet13@hanyang.ac.kr>
2024-06-01 13:46:48 +0900

    dev_main initial

--- build/atomic_configure/_setup_util.py
+++ build/atomic_configure/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/doldolmeng2/test_ws/devel;/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []

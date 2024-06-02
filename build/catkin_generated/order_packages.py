be1672645fdeb91a9f9a238d780e69edff827c0a
doldolmeng2 <ktypet13@hanyang.ac.kr>
2024-06-02 13:33:32 +0900

    merge dev_osy, dev_control

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/doldolmeng2/project2_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/doldolmeng2/project2_ws/devel;/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/doldolmeng2/project2_ws/devel;/home/doldolmeng2/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/doldolmeng2/project2_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/doldolmeng2/project2_ws/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/home/doldolmeng2/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/java8path:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/Program Files/Bandizip/:/mnt/c/Program Files/dotnet/:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python38:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python38/Scripts:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python312:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python312/Scripts:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python38/Scripts/:/mnt/c/Users/ktype/AppData/Local/Programs/Python/Python38/:/mnt/c/Users/ktype/AppData/Local/Microsoft/WindowsApps:/mnt/c/Program Files/JetBrains/IntelliJ IDEA Community Edition 2023.3.2/bin:/mnt/c/Users/ktype/.dotnet/tools:/mnt/c/Users/ktype/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin'
-export PKG_CONFIG_PATH="/home/doldolmeng2/project2_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/doldolmeng2/project2_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/doldolmeng2/project2_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/doldolmeng2/project2_ws/src:$ROS_PACKAGE_PATH"

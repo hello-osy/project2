fd1012bdc5e3374a26b4a5169b75d3a267e18f7e
오상영 <150113262+hello-osy@users.noreply.github.com>
2024-06-05 09:20:15 +0900

    240605
    -각자 작성한 내용을 통합해서 수정중임.
    -오류가 있음. 아직 완성본이 아님.

--- src/image_fuse/src/traffic_lights.py
+++ src/image_fuse/src/traffic_lights.py
@@ -25,7 +25,7 @@ def image_callback(msg):
         height, width = cv_image.shape[:2]
 
         # 오른쪽 위 부분만 관심 영역으로 설정
-        roi = cv_image[0:height//3, (2*width)//3:width]
+        roi = cv_image[height//3:height//2, (2*width)//3:width]
 
         # BGR -> HSV 변환
         roi_img_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
@@ -36,7 +36,7 @@ def image_callback(msg):
 
         # 정의된 색상 범위 내에 있는 픽셀을 추출하고 마스크 생성
         green_mask = cv2.inRange(roi_img_hsv, lower_green, upper_green)
-
+        
         # 초록불 검출 (초록색 픽셀이 100개 이상일 때만)
         green_pixel_count = cv2.countNonZero(green_mask)
         if green_pixel_count > 100:  # 임계값을 100으로 설정
@@ -45,9 +45,10 @@ def image_callback(msg):
 
         # 디버깅 목적으로 주기적으로만 이미지 표시
         if rospy.get_time() % 1 < 0.1:  # 1초마다 한 번씩 표시
-            cv2.imshow("green_mask", green_mask)
-            cv2.imshow("roi", roi_img_hsv)
-
+            # cv2.imshow("green_mask", green_mask)
+            # cv2.imshow("roi", roi_img_hsv)
+            pass
+        
         if cv2.waitKey(1) & 0xFF == 27:  # ESC 키를 누르면 종료
             rospy.signal_shutdown("ESC pressed")
             cv2.destroyAllWindows()
@@ -58,7 +59,7 @@ def image_callback(msg):
 def main():
     global green_light_pub
 
-    rospy.init_node('green_light_detector', anonymous=True)
+    rospy.init_node('green_light_detector')
 
     # 토픽 퍼블리셔 초기화
     green_light_pub = rospy.Publisher('/green_light', Bool, queue_size=1)

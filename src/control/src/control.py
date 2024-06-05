08cabd48568d3e2cfa69f2f63a59f7b555a2ca58
오상영 <150113262+hello-osy@users.noreply.github.com>
2024-06-05 10:08:26 +0900

    240605
    -수정중...

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -50,12 +50,10 @@ class CarController:
     def set_orientation(self, msg):
         self.angle = msg.data  # 현재 조향 업데이트
 
-    @staticmethod
-    def matching(x, input_min, input_max, output_min, output_max):
+    def matching(self, x, input_min, input_max, output_min, output_max):
         return (x - input_min) * (output_max - output_min) / (input_max - input_min) + output_min # map() 함수 정의.
 
-    @staticmethod
-    def car_position(list): # 차체 위치 에러
+    def car_position(self, list): # 차체 위치 에러
         x1, y1, x2, y2, x3, y3, x4, y4 = list
         center_line = (x1 + x3) / 2
         error = (WIDTH / 2) - center_line
@@ -73,13 +71,12 @@ class CarController:
 
         return theta
 
-    @staticmethod
-    def steering_theta(w1, w2): # 차선 기울기 기준 조향
+    def steering_theta(self, w1, w2): # 차선 기울기 기준 조향
         if np.abs(w1) > np.abs(w2):  # 우회전
             if w1 * w2 < 0:  # 정방향 or 약간 틀어진 방향
                 w1 = -w1
                 angle = np.arctan(np.abs(math.tan(w1) - math.tan(w2)) / (1 + math.tan(w1) * math.tan(w2)))
-                theta = CarController.matching(angle, 0, np.pi / 2, 0, 10)
+                theta = self.matching(angle, 0, np.pi / 2, 0, 10)
             elif w1 * w2 > 0:  # 극한으로 틀어진 방향
                 if w1 > w2:
                     theta = 0
@@ -91,7 +88,7 @@ class CarController:
             if w1 * w2 < 0:  # 정방향 or 약간 틀어진 방향
                 w1 = -w1
                 angle = np.arctan(np.abs(math.tan(w1) - math.tan(w2)) / (1 + math.tan(w1) * math.tan(w2)))
-                theta = CarController.matching(angle, 0, np.pi / 2, 0, -10)
+                theta = self.matching(angle, 0, np.pi / 2, 0, -10)
             elif w1 * w2 > 0:  # 극한으로 틀어진 방향
                 if w1 > w2:
                     theta = 0
@@ -123,12 +120,12 @@ class CarController:
             cross_y = left_calculated_weight * ((left_calculated_bias - right_calculated_bias) / (right_calculated_weight - left_calculated_weight)) + right_calculated_bias
             
             if not np.isnan(cross_x) and not np.isnan(cross_y):
-                if -5 < CarController.steering_theta(left_calculated_weight, right_calculated_weight) < 5:
+                if -5 < self.steering_theta(left_calculated_weight, right_calculated_weight) < 5:
                     print('소실점 조향 서보모터 각도: ', self.steering_vanishing_point(cross_x))
                     steering_angle = self.steering_vanishing_point(cross_x)
                 else:
-                    print("기울기 조향 서보모터 각도: ", CarController.steering_theta(left_calculated_weight, right_calculated_weight))
-                    steering_angle = CarController.steering_theta(left_calculated_weight, right_calculated_weight)
+                    print("기울기 조향 서보모터 각도: ", self.steering_theta(left_calculated_weight, right_calculated_weight))
+                    steering_angle = self.steering_theta(left_calculated_weight, right_calculated_weight)
             
         return steering_angle
 
@@ -142,7 +139,7 @@ def lane_callback(msg, args):
 
     theta = car_controller.steering_calcu(msg.data) # msg.data 넣어야 함. 그냥 msg에는 다른 정보도 들어있음.
     # angle = car_controller.compute(theta, dt)
-    speed = 0.0001 # car_controller.control_speed(angle) # 속도 제어 부분은 나중에 수정할 것
+    speed = 0.1 # car_controller.control_speed(angle) # 속도 제어 부분은 나중에 수정할 것
     
     # 각도와 속도를 퍼블리시
     publish_msg = xycar_motor()
